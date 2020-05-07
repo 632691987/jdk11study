ca5f9b6bf7d92a09bcd60f9cdcad0cb113aa31a4
vincent.zhang <v.zhang@aspanetconomy.net>
2020-05-07 17:56:40 +0200

    Interface history

--- src/test/java/org/personal/ch06_interface/ContainPrivateMethodInterface.java
+++ src/test/java/org/personal/ch06_interface/ContainPrivateMethodInterface.java
@@ -1,13 +1,21 @@
 package org.personal.ch06_interface;
 
-public class ContainPrivateMethodInterface {
+public interface ContainPrivateMethodInterface {
 
+	// JDK 7 -
+	String MONGO_DB_NAME = "ABC_Mongo_Datastore";
+	void logInfo(String message);
+
+	// JDK 8
+	// default method and static method
+	default void logWarn(String message){}
+	static void isNull(String str) {}
+
+	// JDK 9
 	private int getPrivate() {
 		return 5;
 	}
-
 	private static int getPrivate2() {
 		return 6;
 	}
-
 }
